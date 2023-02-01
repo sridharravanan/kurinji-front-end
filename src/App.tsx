9fb2f206a3f76691217cee3aa2ab7d6d38933d8d
sridhar <sridharsrds@gmail.com>
2023-02-01 22:57:33 +0530

    Add existing project files to Git

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import '../src/assets/bootstrap/css/bootstrap.min.css';
+import '../src/assets/bootstrap-icons/bootstrap-icons.css';
+import '../src/assets/css/style.css';
+import Login from './pages/Login';
+import Header from './components/Header';
+import SideBar from './components/SideBar';
+import BlankPage from './pages/BlankPage';
 
 function App() {
+  const LoginTemplate = () => <> <main> <Login></Login> </main> </>;
+  const HomeTemplate = () => <> <Header/> <SideBar/> 
+  <main id="main" className="main"> <BlankPage /></main></>;
+  
+  const [isLogin, setIsPreview] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {isLogin ? <LoginTemplate /> : <HomeTemplate />}
+    </>
+    
   );
 }
 
---
+++ src/pages/BlankPage.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+const BlankPage = () => {
+    return (
+        <>
+        <div className="pagetitle">
+        <h1>Blank Page</h1>
+        <nav>
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li className="breadcrumb-item">Pages</li>
+            <li className="breadcrumb-item active">Blank</li>
+          </ol>
+        </nav>
+      </div>
+  
+      <section className="section">
+        <div className="row">
+          <div className="col-lg-6">
+  
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Example Card</h5>
+                <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
+              </div>
+            </div>
+  
+          </div>
+  
+          <div className="col-lg-6">
+  
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Example Card</h5>
+                <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
+              </div>
+            </div>
+  
+          </div>
+        </div>
+      </section>
+      </>
+    );
+}
+export default BlankPage;
